ffbd1efe0942eab2786edf73bb6ac28d3fe8bf51
pushpraj7d <singhpushparaj123@gmail.com>
2025-07-26 00:03:20 +0530

    Added HotelCard and other components, updated Navbar and Home page - 25 July

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import HotelCard from '../components/HotelCard'
+import FeaturedDestination from '../components/FeaturedDestination'
 
 
 const Home = () => {
   return (
     <>
         <Hero/>
+        <FeaturedDestination/>
     </>
   )
 }
