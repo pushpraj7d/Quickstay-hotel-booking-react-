0873804bd27c51c9c9e429a6f32a414bcf93c7b3
pushpraj7d <singhpushparaj123@gmail.com>
2025-07-27 00:05:45 +0530

    Exclusive offer and testimonial updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import HotelCard from '../components/HotelCard'
 import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
 
 
 const Home = () => {
@@ -9,6 +11,8 @@ const Home = () => {
     <>
         <Hero/>
         <FeaturedDestination/>
+        <ExclusiveOffers/>
+        <Testimonial/>
     </>
   )
 }
