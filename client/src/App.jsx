09594c1a0975767c2726f7af27a1d0b70fc97ede
pushpraj7d <singhpushparaj123@gmail.com>
2025-08-04 03:14:47 +0530

    🛠️ Working on HotelOwner page (Dashboard done) – Add Room & List Room pending

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,12 @@ import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
+import MyBookings from './pages/MyBookings';
+import Hotelreg from './components/Hotelreg';
+import Layout from './pages/HotelOwner/Layout';
+import Dashboard from './pages/HotelOwner/Dashboard';
+import AddRoom from './pages/HotelOwner/AddRoom';
+import ListRoom from './pages/HotelOwner/ListRoom';
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,11 +18,18 @@ const App = () => {
   return (
     <div >
       {!isOwnerPath && <Navbar/>}
+      {false && <Hotelreg/>}
       <div className='min-h-[70vh]'>
             <Routes>
               <Route path='/' element={<Home/>}/>
               <Route path='/rooms' element={<AllRooms/>}/>
               <Route path='/rooms/:id' element={<RoomDetails/>}/>
+              <Route path='/my-bookings' element={<MyBookings/>} />
+              <Route path='/owner' element={<Layout/>}>
+                  <Route  index element={<Dashboard/>}/>
+                  <Route  path='add-room' element={<AddRoom/>}/>
+                  <Route  path='list-room' element={<ListRoom/>}/>
+              </Route>
             </Routes>
       </div>
       <Footer/>
